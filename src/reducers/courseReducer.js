eb26ef5ca4f8d92daf80c11b09372698a24c294b
snuffykl <adri4n7@gmail.com>
2017-09-04 15:08:39 +0800

    Add redux to app

---
+++ src/reducers/courseReducer.js
@@ -0,0 +1,11 @@
+// state = [] Default parameter feature in ES6
+import * as types from "../actions/actionTypes";
+
+export default function courseReducer(state = [], action) {
+  switch (action.type) {
+    case types.CREATE_COURSE:
+      return [...state, Object.assign({}, action.course)];
+    default:
+      return state;
+  }
+}
