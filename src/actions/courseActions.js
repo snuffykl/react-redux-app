25d7c34e855903c70074e027f4fcdaf6a656f3c9
snuffykl <adri4n7@gmail.com>
2017-09-04 16:23:16 +0800

    Add redux.thunks and api call to get all data with Presentation Components

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,7 +1,20 @@
 // Action creator
 
 import * as types from "./actionTypes";
+import courseApi from "../api/mockCourseApi";
 
-export function createCourse(course) {
-  return {type: types.CREATE_COURSE, course};
+export function loadCoursesSucess(courses) {
+  return {type: types.LOAD_COURSES_SUCCESS, courses};
+}
+
+export function loadCourses(){
+     return function(dispatch){
+       return courseApi.getAllCourses()
+         .then(courses => {
+           dispatch(loadCoursesSucess(courses));
+         })
+         .catch(error => {
+           throw(error);
+         });
+     };
 }
